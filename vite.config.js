d4ea170aaf4d3d1e67101f02ed84750a00775e18
zuxaor <adrienfemenia8@gmail.com>
2026-09-13 03:07:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Milo-Studio/",
   plugins: [react()],
-  base: "./",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets"
-  }
 });
